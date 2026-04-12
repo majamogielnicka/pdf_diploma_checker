8e5c28ce9227ff362787a16b8c00764cd7a31d9a
MichalProc <michal.proc13@gmail.com>
2026-04-12 14:37:02 +0200

    Fixed and optimized example_usage.py

--- src/analysis/extraction/example_usage.py
+++ src/analysis/extraction/example_usage.py
@@ -18,38 +18,59 @@ output_path = PROJECT_ROOT / "src" / "output.json"
 # TODO: dodać więcej przykładowych plików pdf do folderu /redaction_debug
 # Format nazwy pdfa: <aspekt_do_sprawdzenia>_example.pdf
 debug_mode = 1
-debug_type = "blankpages" # zmiana trybu debugowania (wpisać interesujący nas aspekt)
+debug_type = "table" # zmiana trybu debugowania (wpisać interesujący nas aspekt)
 debug_path = str(PROJECT_ROOT / "src" / "analysis" / "extraction" / "redaction_debug" / "{debug_type}_example.pdf")
-
-
-#test:
-#print(extractPDF("1.pdf").to_dict())
 
 if debug_mode == 0:
     pdf_path = Path(input_path)
 elif debug_mode == 1:
-    candidate = Path(debug_path.format(debug_type="blankpages"))
+    candidate = Path(debug_path.format(debug_type=debug_type))
     if candidate.exists():
         pdf_path = candidate
     else:
-        print(f"Debug PDF not found: {candidate}. Falling back to default thesis path.")
-        pdf_path = PROJECT_ROOT / "src" / "theses" / "gp.pdf"
+        print(f"[extraction_json] Debug PDF not found: {candidate}. Falling back to default thesis path.")
+        pdf_path = Path(input_path)
 
 doc_data = extractPDF(str(pdf_path))
 
 #TODO: dodac warunek sprqwdzjaacy blad do testow
 if doc_data is not None:
     doc_data.to_json(output_path) 
+    print("[extraction_json] JSON wygenerowany")
+else:
+    print(f"[extraction_json] Nie wygenerowano pliku JSON") 
+
 
 # -----------------------przykład dla converter_linguistics.py-----------------------
-from pathlib import Path
 from converter_linguistics import PDFMapper
-pdf_path = input_path
 
-if pdf_path.exists():
-    raw_data = extractPDF(str(pdf_path))
-    final_doc = PDFMapper.map_to_schema(raw_data)
-    final_doc.to_json("pdf_diploma_checker/output_linguistics.json")
-    print("JSON wygenerowany")
+output_path_linguistics = PROJECT_ROOT / "src" / "output_linguistics.json"
+doc_data_linguistics = doc_data # Domyślnie mapowany jest plik wygenerowany przez extraction_json.py
+
+# Odkomentować ponizej w celu osobnego potraktowania extraction_json i converter_linguistics
+'''
+input_path_linguistics = PROJECT_ROOT / "data" / "zusz.pdf"
+
+debug_mode_linguistics = 1
+debug_type_linguistics = "table" # zmiana trybu debugowania (wpisać interesujący nas aspekt)
+debug_path_linguistics = str(PROJECT_ROOT / "src" / "analysis" / "extraction" / "redaction_debug" / "{debug_type}_example.pdf")
+
+if debug_mode_linguistics == 0:
+    pdf_path_linguistics = Path(input_path_linguistics)
+elif debug_mode_linguistics == 1:
+    candidate_linguistics = Path(debug_path_linguistics.format(debug_type_linguistics=debug_type_linguistics))
+    if candidate_linguistics.exists():
+        pdf_path_linguistics = candidate
+    else:
+        print(f"[converter_linguistics] Debug PDF not found: {candidate_linguistics}. Falling back to default thesis path.")
+        pdf_path_linguistics = Path(input_path)
+
+doc_data_linguistics = extractPDF(str(pdf_path_linguistics))
+'''
+
+if doc_data_linguistics is not None:
+    final_doc = PDFMapper.map_to_schema(doc_data)
+    final_doc.to_json(output_path_linguistics)
+    print("[converter_linguistics] JSON wygenerowany")
 else:
-    print(f"Błąd: Nie znaleziono pliku {pdf_path}") 
+    print(f"[converter_linguistics] Nie wygenerowano pliku JSON") 
