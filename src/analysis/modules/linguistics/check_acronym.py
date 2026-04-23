2ef673e0e2a3f64fbdf08ba3716c58772d1c425d
zosianarloch <zosia_narloch@wp.pl>
2026-04-23 20:49:29 +0200

    Add minor fixes in acronyms

--- src/analysis/modules/linguistics/check_acronym.py
+++ src/analysis/modules/linguistics/check_acronym.py
@@ -8,6 +8,9 @@ def potential_acronym(text):
     "STRESZCZENIE", "ABSTRACT", "SŁOWA", "KLUCZOWE",
     "KEYWORDS", "WYKAZ", "SKRÓTÓW", "ABBREVIATIONS",
     "ENGINEERING", "THESIS", "UNIVERSITY", "POLITECHNIKA",
+    "WSTĘP", "CEL", "PRACY", "TEORIA", "PRZEGLĄD", "ROZWIĄZAŃ", "OPIS", 
+    "WYNIKI", "ZAKOŃCZENIE", "DODATEK", "INSTRUKCJA", "PROGRAMISTY", 
+    "DYPLOMU", "UŻYTKOWNIKA", "BIBLIOGRAFIA", "SPIS", "TREŚCI", "RYSUNKÓW", "TABEL"
     }
     clean_text = text.strip("():;,.!?[]\n\t \"„”«»“‟‘’")
     if re.match(r'^([A-Z]\.){1,}[A-Z]?$', clean_text):
@@ -48,29 +51,36 @@ def check_if_was_defined(blocks, acronyms_with_definitions, proper_names):
         "USA", "EU", "UN", "NATO", "WHO", "UNESCO", "ONZ", "UE", "PL", "EN", 
         "IT", "PC", "USB", "GPS", "WiFi", "PDF", "PhD", "MSc", "BSc", "SI", "CEO"
     }
-
     QUOTE_MARKS = {'"', '„', '”', '«', '»', '“', '‟', '‘', '’'}
     category = "ACRONYM_UNDEFINED"
     message = "Skrót nie został zdefiniowany przed jego użyciem."
     matches = []
     reported_acronyms = set()
-    proper_names_clean = {p[0].strip("() \n\t.,;:") for p in proper_names if not p[0].strip("() \n\t.,;:").isupper()} if proper_names else set()
+    no_parenthesis = re.compile(r'\b([A-Z]{2,})\s*\((?:ang\.|pol\.|fr\.)\s+([^)]{3,}?)(?=[,;]|\s{2,}[A-Z]{2})', re.UNICODE)
+    roman_numeral = re.compile(r'^M{0,4}(CM|CD|D?C{0,3})(XC|XL|L?X{0,3})(IX|IV|V?I{0,3})$')
+    #proper_names_clean = {p[0].strip("() \n\t.,;:") for p in proper_names if not p[0].strip("() \n\t.,;:").isupper()} if proper_names else set()
     
     for block in blocks:
         block = block.block
         if block.type in {"paragraph", "list"}:
             for word in block.words:
                 text = word.text
-                clean_text = text.strip("():;,.!?[]\n\t \"„”«»“‟‘’")
+                clean_text = text.strip("|():;,.!?[]\n\t \"„”«»“‟‘’")
                 if any(c in QUOTE_MARKS for c in text):
                     continue
                 if not potential_acronym(text):
                     continue
-                if clean_text in proper_names_clean:
+                if clean_text[0].isdigit():
                     continue
+                if roman_numeral.match(clean_text):
+                    continue
+                # if clean_text in proper_names_clean:
+                #     continue
                 if clean_text in GLOBAL_ACRONYMS:
                     continue
                 page = word.page_number
+                if no_parenthesis.search(block.content[word.start_char:word.start_char:]):
+                    continue   
                 if clean_text in acronyms_with_definitions:
                     acronym = acronyms_with_definitions[clean_text]
                     acronym_page, acronym_bbox = acronym[2], acronym[3]
--- src/analysis/modules/linguistics/proper_names.py
+++ src/analysis/modules/linguistics/proper_names.py
@@ -87,5 +87,4 @@ def get_proper_names(blocks):
                         keyword_lemma, is_found = lemmatization(keyword, block.language)
                     keywords_lemma.append((keyword, keyword_lemma))
                 proper_names.extend(keywords_lemma)
-    proper_names = set(proper_names)
     return proper_names
