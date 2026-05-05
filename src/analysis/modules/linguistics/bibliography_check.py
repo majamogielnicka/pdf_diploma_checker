22671ea875497ac832206eb96760a56289aeb217
sloma <aleksandra.slomskaa@gmail.com>
2026-05-05 11:09:31 +0200

    updates to patterns

--- src/analysis/modules/linguistics/bibliography_check.py
+++ src/analysis/modules/linguistics/bibliography_check.py
@@ -2,7 +2,7 @@ from helpers import nlp_en
 import re
 '''
 Skrypt sprawdzający kompletność bibliografii i sprawdzający jej zgodność z wybranym stylem bibliograficznym: 
-(pn-iso 690:2012, APA, IEEE, Harvard)
+docelowo: (pn-iso 690:2012, APA 7, IEEE, Harvard 4)
 '''
 
 def check_bibliography(blocks):
@@ -14,33 +14,60 @@ def check_bibliography(blocks):
     '''
 
     date_patterns = {
-        r'\d{1,2}/\d{1,2}/[12]\d{3}\b':          'dd/mm/yyyy',
-        r'[12]\d{3}/\d{1,2}/\d{1,2}\b':          'yyyy/mm/dd',
-        r'\d{1,2}-\d{1,2}-[12]\d{3}\b':          'dd-mm-yyyy',
-        r'\b[12]\d{3}-\d{1,2}-\d{1,2}\b':        'yyyy-mm-dd',
-        r'\d{1,2}\.\d{1,2}\.[12]\d{3}\b':        'dd.mm.yyyy',
-        r'\b[12]\d{3}\.\d{1,2}\.\d{1,2}':        'yyyy.mm.dd',
-        r'(?<![\w\/\.\-])[12]\d{3}(?![\w\/\-])':   'yyyy',
+        r'(?<![\w\/\.\-])(?<!\d\.)\d{1,2}/\d{1,2}/[12]\d{3}\b(?![\w\/\-])(?!\.\d)': 'dd/mm/yyyy',
+        r'(?<![\w\/\.\-])(?<!\d\.)[12]\d{3}/\d{1,2}/\d{1,2}\b(?![\w\/\-])(?!\.\d)': 'yyyy/mm/dd',
+        r'(?<![\w\/\.\-])(?<!\d\.)\d{1,2}-\d{1,2}-[12]\d{3}\b(?![\w\/\-])(?!\.\d)': 'dd-mm-yyyy',
+        r'(?<![\w\/\.\-])(?<!\d\.)\b[12]\d{3}-\d{1,2}-\d{1,2}\b(?![\w\/\-])(?!\.\d)': 'yyyy-mm-dd',
+        r'(?<![\w\/\.\-])(?<!\d\.)\d{1,2}\.\d{1,2}\.[12]\d{3}\b(?![\w\/\-])(?!\.\d)': 'dd.mm.yyyy',
+        r'(?<![\w\/\.\-])(?<!\d\.)\b[12]\d{3}\.\d{1,2}\.\d{1,2}\b(?![\w\/\-])(?!\.\d)': 'yyyy.mm.dd',
+        r'(?<![\w\/\.\-])(?<!\d\.)\b[12]\d{3}\b(?![\w\/\-])(?!\.\d)': 'yyyy',
     }
 
     doi_patterns = {
-        r'doi\.org/10\.\d{4,9}': 'link',
-        r'(DOI|doi)(\s*):\s*10\.\d{4,9}': 'citation',
+        r'doi\.org/10\.\d{4,9}/\S+': 'link',
+        r'(?:DOI|doi)\s*:\s*10\.\d{4,9}/\S+': 'citation',
     }
-    #TODO: suffixes
+
+    volume_patterns = {
+        r'\d{1,4}\(\d{1,4}(?:-\d{1,4})?\)': '2(14)',
+        r'vol\.\s*\d{1,4}': 'vol. 2',
+        r'volume[\s\:]*\d{1,4}' : 'volume 2',
+        r'\(\d{1,4}\)[\s:]*\d+(?:\s*[-–]\s*\d+)?': '(2):144 - 158' #miks bibtex
+    }
+
+    page_range_patterns = {
+        r'(?<![\d\.\w])pp\.[\s\:]*\d+(?:\s*[-–]\s*\d+)?': 'pp. 2 - 4',
+        r'(?<![\d\.\w])s\.[\s\:]*\d+(?:\s*[-–]\s*\d+)?': 's. 2 - 4',
+        r'(?<![\d\.\w])pages?[\s\:]*\d+(?:\s*[-–]\s*\d+)?' :'pages 2 - 4',
+        r'(?<![\d\.\w])stron[ya][\s\:]*\d+(?:\s*[-–]\s*\d+)?':'strony 2 - 4',
+        r'(?<![\d\.\w])\(\d{1,4}\)[\s:]*\d+(?:\s*[-–]\s*\d+)?': '(2):2 - 4', #mix bibtex
+        r'(?<![\w\/\.\-])\d+\s*[-–]\s*\d+(?![\w\/\-])': 'bare 2 - 4' #złapie też inne, może if jeśli nie są inne ewentualnie dodatkowe lookbehind
+    }
+
+    access_date_pattern = {
+        r'(?<![\d\.\w])[aA]vailable\s+[aA]t[\s:]*' : 'Available at:',
+        r'(?<![\d\.\w])\[\s*[oO]nline\s*\]\s*': '[online]',
+        r'(?<![\d\.\w])[oO]nline\s*[aA]t[\s\:]*': 'online',
+        r'(?<![\d\.\w])[dD]ost[eę]p[\s\:]*': 'dostęp',
+        r'(?<![\d\.\w])[dD]ata\s*[dD]ostępu[\s\:]*': 'data dostępu',
+        r'(?<![\d\.\w])\(\s*[dD]ata\s*[dD]ostępu[\s\:]*\)': '(data dostępu ...)'
+    }
+
     UPPER_CASE = r'[A-ZĄĆĘŁŃÓŚŹŻÀ-ÖØ-öø-ÿĀ-ž]'
     LOWER_CASE = r'[a-ząćęłńóśźżà-öø-ÿā-ž]'
-    special_chars  = rf"([–-'])"
-    surname_pattern = rf'{UPPER_CASE}{LOWER_CASE}+({special_chars}({UPPER_CASE}|{LOWER_CASE})+)*'
+    special_chars  = rf"[-–']"
+    surname_pattern = rf'{UPPER_CASE}{LOWER_CASE}+(?:{special_chars}(?:{UPPER_CASE}|{LOWER_CASE})+)*'
     fullname_pattern = rf'{UPPER_CASE}{LOWER_CASE}+'
     short_name_pattern = rf'{UPPER_CASE}\.'
 
     author_patterns = {
-        rf'{short_name_pattern}(\s*{short_name_pattern})*\s{surname_pattern}': 'J. Nowak',
-        rf'{fullname_pattern}(\s{fullname_pattern})*\s{surname_pattern}': 'Jan Nowak',
-        rf'{surname_pattern},\s{short_name_pattern}(\s*{short_name_pattern})*': 'Nowak, J.',
-        rf'{surname_pattern},\s{fullname_pattern}(\s{fullname_pattern})*': 'Nowak, Jan',
+        rf'{short_name_pattern}(?:\s*{short_name_pattern})*\s{surname_pattern}': 'J. Nowak',
+        rf'{fullname_pattern}(?:\s{fullname_pattern})*\s{surname_pattern}': 'Jan Nowak',
+        rf'{surname_pattern},\s{short_name_pattern}(?:\s*{short_name_pattern})*': 'Nowak, J.',
+        rf'{surname_pattern},\s{fullname_pattern}(?:\s{fullname_pattern})*': 'Nowak, Jan',
     }
 
-    #check_title
+    
+    #check_quotes
+    #check_illiadic
     return matches
