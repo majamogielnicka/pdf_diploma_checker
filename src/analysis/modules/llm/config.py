7591e747d0e5f23156f37eb14ec5e797d49087f3
Tomek <tomekpaw44@gmail.com>
2026-05-12 21:07:08 +0200

    Syncing configs with branch exe before merge

--- src/analysis/modules/llm/config.py
+++ src/analysis/modules/llm/config.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from pathlib import Path
 
 """
@@ -11,15 +13,21 @@ wraz ze sprawdzeniem poprawności ścieżek
 BASE_DIR = Path(__file__).resolve().parent
 PROJECT_ROOT = BASE_DIR.parents[3]
 SRC_DIR = PROJECT_ROOT / "src"
+
+for p in (PROJECT_ROOT, SRC_DIR):
+    p_str = str(p)
+    if p_str not in sys.path:
+        sys.path.insert(0, p_str)
+
+from common.path import resource_path
+
 EMBEDDING_MODEL = "intfloat/multilingual-e5-large"
-OUTPUT_DIR = PROJECT_ROOT / "src" / "analysis" / "modules" / "llm" / "wyniki"
+OUTPUT_DIR = Path(resource_path(os.path.join("analysis", "modules", "llm", "wyniki")))
 
 #JEDYNE 3 LINIJKI DO ZMIANY, JEŚLI URUCHAMIASZ
 # MODEL_PATH = Path.home() / "models" / "gemma3_12b" / "google_gemma-3-12b-it-Q4_K_M.gguf"
-MODEL_PATH = SRC_DIR / "models" / "gemma-3-12b-it-Q4_K_M.gguf"
-LLAVA_MODEL_PATH = SRC_DIR / "models" / "llava-v1.6-mistral-7b.Q4_K_M.gguf"
-LLAVA_MMPROJ_PATH = SRC_DIR / "models" / "mmproj-model-f16.gguf"
-THESIS_PATH = PROJECT_ROOT / "data" / "most_important" / "jago.pdf"
+MODEL_PATH = Path.home() / "models" / "gemma3_12b" / "google_gemma-3-12b-it-Q4_K_M.gguf"
+THESIS_PATH = Path(resource_path(os.path.join("..", "data", "most_important", "jabi.pdf")))
 LANGUAGE = "pl" #LUB "en"
 
 if __name__ == "__main__":
