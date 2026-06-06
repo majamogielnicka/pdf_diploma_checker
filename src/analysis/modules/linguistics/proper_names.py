73bebab5670b8fd23b37ddb0612577c2837b9e8c
zosianarloch <zosia_narloch@wp.pl>
2026-06-06 17:52:02 +0200

    Add additional changes for words in italics and acronyms with numbers or dash

--- src/analysis/modules/linguistics/proper_names.py
+++ src/analysis/modules/linguistics/proper_names.py
@@ -130,4 +130,10 @@ def get_proper_names(blocks):
                 word_lemma, _ = lemmatization(word, block.language)
                 proper_names.append((word, word_lemma))
 
+        if block.block.type == "paragraph":
+            for word in block.block.words:
+                if word.italic:
+                    text = word.text
+                    word_lemma, _ = lemmatization(text, block.language)
+                    proper_names.append((text, word_lemma))
     return proper_names, bibliography
